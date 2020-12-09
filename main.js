7f12664ab285003b7c3d386b130c0d20442749b1
karthikramss2428 <karthikramss2428@gmail.com>
2020-12-09 20:41:48 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,70 @@
+
+function whatsapp()
+{
+    alert("Contact us on whatsapp: 9385608175");
+}
+function instagram()
+{
+    alert("Follow us on Instagram: shoppingcorner.1234")
+}
+function twitter()
+{
+    alert("Follow us on twitter: #shoppingcorner")
+}
+function facebook()
+{
+    alert("Follow our facebook page: ShopCorner")
+}
+function img1()
+{
+    document.getElementById('one').src='r7pro.jpg';
+}
+function img2()
+{
+    document.getElementById('one').src='pocox3.jpeg';
+}
+function img3()
+{
+    document.getElementById('one').src='m51.jpg';
+}
+function img4()
+{
+    document.getElementById('one').src='pic9.jpg';
+}
+function img5()
+{
+    document.getElementById('one').src='oneplus8.jpg';
+}
+function move()
+{
+    var image= document.getElementById("one").src;
+    if(image==="file:///E:/coding%20mart/Coding%20Mart/Mobile%20shop/r7pro.jpg")
+    {
+        location.href="check.html";
+    }
+
+}
+function oneplus()
+{
+    location.href="project.html";
+}
+function samsung()
+{
+    location.href="project1.html";
+}
+function poco()
+{
+    location.href="project2.html";
+}
+function vivo()
+{
+    location.href="project3.html";
+}
+function redmi()
+{
+    location.href="project4.html";
+}
+function realme()
+{
+    location.href="project5.html"
+}
